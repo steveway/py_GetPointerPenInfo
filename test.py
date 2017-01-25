daf238d5f4d994f552077b28a9cdad87c1461e0a
Stefan Murawski <steveway1@googlemail.com>
2017-01-25 15:58:40 +0100

    A few improvements to better understand this whole chaos.

--- test.py
+++ test.py
@@ -12,11 +12,12 @@ GetPointerPenInfo = windll.user32.GetPointerPenInfo
 
 GET_POINTERID_WPARAM = LOWORD
 (PT_POINTER,
-PT_TOUCH,
-PT_PEN,
-PT_MOUSE,
-PT_TOUCHPAD)=map(c_int,xrange(1,6))
- 
+ PT_TOUCH,
+ PT_PEN,
+ PT_MOUSE,
+ PT_TOUCHPAD) = map(c_int, xrange(1, 6))
+PointerInputTypes = ["PT_POINTER", "PT_TOUCH", "PT_PEN", "PT_MOUSE", "PT_TOUCHPAD"]
+
 '''
   POINTER_INPUT_TYPE         pointerType;
   UINT32                     pointerId;
@@ -67,7 +68,8 @@ class POINTER_INFO(Structure):
   INT32        tiltX;
   INT32        tiltY;
 '''    
-    
+
+
 class POINTER_PEN_INFO(Structure):
     _fields_ = [
         ('pointerInfo', POINTER_INFO),
@@ -92,11 +94,14 @@ class WNDCLASS(Structure):
                 ('lpszMenuName', c_char_p),
                 ('lpszClassName', c_char_p)]
 '''
+
+
 class RECT(Structure):
     _fields_ = [('left', c_long),
                 ('top', c_long),
                 ('right', c_long),
                 ('bottom', c_long)]
+
 
 class PAINTSTRUCT(Structure):
     _fields_ = [('hdc', c_int),
@@ -106,10 +111,12 @@ class PAINTSTRUCT(Structure):
                 ('fIncUpdate', c_int),
                 ('rgbReserved', c_char * 32)]
 
+
 class POINT(Structure):
     _fields_ = [('x', c_long),
                 ('y', c_long)]
-    
+
+
 class MSG(Structure):
     _fields_ = [('hwnd', c_int),
                 ('message', c_uint),
@@ -118,11 +125,35 @@ class MSG(Structure):
                 ('time', c_int),
                 ('pt', POINT)]
 
+# typedef struct tagPOINTER_TOUCH_INFO {
+#   POINTER_INFO pointerInfo;
+#   Touch Flags  touchFlags;
+#   Touch Mask   touchMask;
+#   RECT         rcContact;
+#   RECT         rcContactRaw;
+#   UINT32       orientation;
+#   UINT32       pressure;
+# } POINTER_TOUCH_INFO;
+
+
+class POINTER_TOUCH_INFO(Structure):
+    _fields_ = [
+        ('pointerInfo', POINTER_INFO),
+        ('touchFlags', c_int),
+        ('touchMask', c_int),
+        ('rcContact', RECT),
+        ('rcContactRaw', RECT),
+        ('orientation', c_uint32),
+        ('pressure', c_uint32),
+    ]
+
+
 def ErrorIfZero(handle):
     if handle == 0:
         raise WinError
     else:
         return handle
+
 
 def MainWin():
     CreateWindowEx = windll.user32.CreateWindowExA
@@ -133,81 +164,79 @@ def MainWin():
     wndclass = WNDCLASS()
     wndclass.style = win32con.CS_HREDRAW | win32con.CS_VREDRAW
     wndclass.lpfnWndProc = WNDPROC(WndProc)
-    #wndclass.cbClsExtra = 
+    # wndclass.cbClsExtra =
     wndclass.cbWndExtra = 0
     wndclass.hInstance = win32api.GetModuleHandle(None)
     wndclass.hIcon = LoadIcon(c_int(win32con.NULL).value,c_int(win32con.IDI_APPLICATION).value)
-    #wndclass.hIcon = windll.user32.LoadIconA(c_int(win32con.NULL), c_int(win32con.IDI_APPLICATION))
+    # wndclass.hIcon = windll.user32.LoadIconA(c_int(win32con.NULL), c_int(win32con.IDI_APPLICATION))
     wndclass.hCursor = LoadCursor(c_int(win32con.NULL).value, c_int(win32con.IDC_ARROW).value)
-    #wndclass.hCursor = windll.user32.LoadCursorA(c_int(win32con.NULL), c_int(win32con.IDC_ARROW))
+    # wndclass.hCursor = windll.user32.LoadCursorA(c_int(win32con.NULL), c_int(win32con.IDC_ARROW))
     wndclass.hbrBackground = GetStockObject(c_int(win32con.WHITE_BRUSH).value)
-    #wndclass.hbrBackground = windll.gdi32.GetStockObject(c_int(win32con.WHITE_BRUSH))
+    # wndclass.hbrBackground = windll.gdi32.GetStockObject(c_int(win32con.WHITE_BRUSH))
     wndclass.lpszMenuName = ""
     wndclass.lpszClassName = "MainWin"
     # Register Window Class
     
     RegisterClass(wndclass)
     
-    #if not RegisterClass(wndclass):
+    # if not RegisterClass(wndclass):
     #    raise WinError()
-    #if not windll.user32.RegisterClassA(byref(wndclass)):
+    # if not windll.user32.RegisterClassA(byref(wndclass)):
     #    raise WinError()
     # Create Window
     print(type(win32con.NULL))
-    hwnd = CreateWindow (
-                          wndclass.lpszClassName,
-                          "Python Window",
-                          win32con.WS_OVERLAPPED | win32con.WS_VISIBLE,
-                          0,
-                          0,
-                          win32con.CW_USEDEFAULT,
-                          win32con.CW_USEDEFAULT,
-                          0,
-                          0,
-                          wndclass.hInstance,
-                          None
-                          )
+    hwnd = CreateWindow(wndclass.lpszClassName,
+                        "Python Window",
+                        win32con.WS_OVERLAPPED | win32con.WS_VISIBLE,
+                        0,
+                        0,
+                        win32con.CW_USEDEFAULT,
+                        win32con.CW_USEDEFAULT,
+                        0,
+                        0,
+                        wndclass.hInstance,
+                        None
+                        )
     print("test1")
     # hwnd = CreateWindowEx(0,
-                          # wndclass.lpszClassName,
-                          # "Python Window",
-                          # win32con.WS_OVERLAPPEDWINDOW,
-                          # win32con.CW_USEDEFAULT,
-                          # win32con.CW_USEDEFAULT,
-                          # win32con.CW_USEDEFAULT,
-                          # win32con.CW_USEDEFAULT,
-                          # win32con.NULL,
-                          # win32con.NULL,
-                          # wndclass.hInstance,
-                          # win32con.NULL)
-    
-    
-    
+    #                       wndclass.lpszClassName,
+    #                       "Python Window",
+    #                       win32con.WS_OVERLAPPEDWINDOW,
+    #                       win32con.CW_USEDEFAULT,
+    #                       win32con.CW_USEDEFAULT,
+    #                       win32con.CW_USEDEFAULT,
+    #                       win32con.CW_USEDEFAULT,
+    #                       win32con.NULL,
+    #                       win32con.NULL,
+    #                       wndclass.hInstance,
+    #                       win32con.NULL)
+
     # Show Window
-    #ShowWindow(hwnd, win32con.SW_SHOWNORMAL)
+    # ShowWindow(hwnd, win32con.SW_SHOWNORMAL)
     print("test2")
-    UpdateWindow(hwnd)
+    # UpdateWindow(hwnd)
     print("test3")
-    SetWindowPos(hwnd, win32con.HWND_TOPMOST, -1200, 10, 500, 500, win32con.SWP_SHOWWINDOW)
-    #SetWindowPlacement(hwnd,(0,0,(0,0),(0,0),(50,20,500,500)))
+    # SetWindowPos(hwnd, win32con.HWND_TOPMOST, -1200, 10, 500, 500, win32con.SWP_SHOWWINDOW)
+    # SetWindowPlacement(hwnd,(0,0,(0,0),(0,0),(50,20,500,500)))
     # Pump Messages
-    #msg = MSG()
-    #pMsg = pointer(msg)
-    #NULL = c_int(win32con.NULL)
-    print("EnableMousePointerMessage: ",windll.user32.EnableMouseInPointer(BOOL(True)))
+    msg = MSG()
+    pMsg = pointer(msg)
+    NULL = c_int(win32con.NULL)
+    print("EnableMousePointerMessage: ", windll.user32.EnableMouseInPointer(BOOL(True)))
     print("Enabled?: ", windll.user32.IsMouseInPointerEnabled())
-    sucval,pMsg = GetMessage(hwnd,0,0)
+    sucval, pMsg = GetMessage(hwnd,0,0)
     while sucval:
-        sucval,pMsg = GetMessage(hwnd,0,0)
-        #print(pMsg)
+        sucval, pMsg = GetMessage(hwnd,0,0)
+        print(pMsg)
         TranslateMessage(pMsg)
         DispatchMessage(pMsg)
     # while windll.user32.GetMessageA( pMsg, NULL, 0, 0) != 0:
         # windll.user32.TranslateMessage(pMsg)
         # windll.user32.DispatchMessageA(pMsg)
 
-    #return pMsg.wParam
-    
+    # return pMsg.wParam
+
+
 def WndProc(hwnd, message, wParam, lParam):
     penstruct = POINTER_PEN_INFO()
     pointpenst = pointer(penstruct)
@@ -216,32 +245,34 @@ def WndProc(hwnd, message, wParam, lParam):
     WM_POINTERUPDATE = 0x0245
     if message == win32con.WM_PAINT:
         hdc, ps = BeginPaint(hwnd)
-        #hdc = windll.user32.BeginPaint(c_int(hwnd), byref(ps))
+        # hdc = windll.user32.BeginPaint(c_int(hwnd), byref(ps))
         rect = GetClientRect(hwnd)
-        #windll.user32.GetClientRect(c_int(hwnd), byref(rect))
-        DrawText(hdc, "Python Powered by Windows", -1,rect,win32con.DT_SINGLELINE|win32con.DT_CENTER|win32con.DT_VCENTER)
+        # windll.user32.GetClientRect(c_int(hwnd), byref(rect))
+        DrawText(hdc, "Python Powered by Windows", -1, rect,
+                 win32con.DT_SINGLELINE | win32con.DT_CENTER | win32con.DT_VCENTER)
         # windll.user32.DrawTextA(c_int(hdc),
                                 # "Python Powered Windows" ,
                                 # c_int(-1), byref(rect), 
                                 # win32con.DT_SINGLELINE|win32con.DT_CENTER|win32con.DT_VCENTER)
         EndPaint(hwnd,ps)
-        #windll.user32.EndPaint(c_int(hwnd), byref(ps))
+        # windll.user32.EndPaint(c_int(hwnd), byref(ps))
         return 0
     elif message == WM_POINTERUPDATE:
         pointerID = GET_POINTERID_WPARAM(wParam)
-        if GetPointerPenInfo(pointerID,pointpenst) != 0:
-            print("X: ",penstruct.pointerInfo.ptPixelLocation.x,"\nY: ",penstruct.pointerInfo.ptPixelLocation.y)
-            print("Pressure: ",penstruct.pressure)
-        #print("yo")
+        if GetPointerPenInfo(pointerID, pointpenst) != 0:
+            print("X: ", penstruct.pointerInfo.ptPixelLocation.x, "\nY: ", penstruct.pointerInfo.ptPixelLocation.y)
+            print("Pressure: ", penstruct.pressure)
+            print("Type: ", PointerInputTypes[penstruct.pointerInfo.pointerType-1])
+        # print("yo")
     elif message == win32con.WM_DESTROY:
         PostQuitMessage(0)
-        #windll.user32.PostQuitMessage(0)
+        # windll.user32.PostQuitMessage(0)
         return 0
 
     return DefWindowProc(c_int(hwnd).value, 
-                        c_int(message).value, 
-                        c_int(wParam).value, 
-                        c_long(lParam).value)
+                         c_int(message).value,
+                         c_int(wParam).value,
+                         c_long(lParam).value)
 
-if __name__=='__main__':
-    sys.exit(MainWin())
+if __name__ == '__main__':
+    MainWin()
